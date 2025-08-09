09e4bf8cfadb7df32264215d6fbe61968f2a20f4
BHARAT <kingaggarwal4197@gmail.com>
2025-08-09 19:35:14 +0530

    cloudinary and multer

---
+++ src/utils/cloudanary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudanary} from 'cloudinary';
+import { log } from 'console';
+import fs from "fs"
+
+cloudanary.config({ 
+        cloud_name: process.env.CLOUDANARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDANARY_API_KEY, 
+        api_secret: process.env.CLOUDANARY_API_SECRET       
+    })
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if(!localfilepath) return null
+        const response = await cloudanary.uploader.upload(localfilepath, {
+            resource_type: 'auto'
+        })
+        console.log(`file uploaded successfully : ${response.url}`);
+        return response        
+    } catch (error) {
+        fs.unlinkSync(localfilepath) 
+        return null
+        
+    }
+
+}
+
+
+export default uploadOnCloudinary
